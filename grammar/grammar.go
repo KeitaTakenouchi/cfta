0210198bb267ef07ae6cf6426a63609abfaaff64
Takenouchi Keita <t.keita.osaka@gmail.com>
2018-11-21 00:11:25 +0900

    Implement functions about grammar.

--- grammar/grammar.go
+++ grammar/grammar.go
@@ -1,6 +1,7 @@
 package grammar
 
 import (
+	"fmt"
 	"strconv"
 )
 
@@ -8,7 +9,58 @@ import (
 type Grammar struct {
 	terminals       []Token
 	nonTerminals    []NonTerminalSymbol
-	productionRules map[NonTerminalSymbol]rhs
+	productionRules map[NonTerminalSymbol][]rightHandSide
+	startSymbol     []NonTerminalSymbol
+}
+
+// NewGrammar is a  constructor of Grammar.
+func NewGrammar() *Grammar {
+	g := &Grammar{
+		terminals:       make([]Token, 0),
+		nonTerminals:    make([]NonTerminalSymbol, 0),
+		productionRules: make(map[NonTerminalSymbol][]rightHandSide),
+		startSymbol:     make([]NonTerminalSymbol, 0),
+	}
+	return g
+}
+
+// AddProdoctionRule adds a prodoction rule to the grammar.
+func (g *Grammar) AddProdoctionRule(left NonTerminalSymbol, right Token, args ...Symbol) {
+	if right.Arity != len(args) {
+		msg := fmt.Sprintf("arith is %d but length of args is %d", right.Arity, len(args))
+		panic(msg)
+	}
+
+	rules, ok := g.productionRules[left]
+	if !ok {
+		rules = make([]rightHandSide, 0)
+	}
+	rhs := newRightHandSide(right, args...)
+	g.productionRules[left] = append(rules, rhs)
+}
+
+// SetStartSymbol sets a symbol as a topmost one of the grammar.
+func (g *Grammar) SetStartSymbol(symbol NonTerminalSymbol) {
+	g.startSymbol = append(g.startSymbol, symbol)
+}
+
+// CreateToken is a constructor of Symbol.
+func (g *Grammar) CreateToken(symbol string, arity int) Token {
+	tk := Token{
+		Symbol{symbol},
+		arity,
+	}
+	g.terminals = append(g.terminals, tk)
+	return tk
+}
+
+// CreateNonTerminalSymbol is a constructor of NonTerminalSymbol.
+func (g *Grammar) CreateNonTerminalSymbol(symbol string) NonTerminalSymbol {
+	s := NonTerminalSymbol{
+		Symbol{symbol},
+	}
+	g.nonTerminals = append(g.nonTerminals, s)
+	return s
 }
 
 // Symbol is a symbol in DSL.
@@ -16,25 +68,9 @@ type Symbol struct {
 	Text string
 }
 
-// NewSymbol is a constructor of Symbol.
-func NewSymbol(symbol string) Symbol {
-	return Symbol{
-		Text: symbol,
-	}
-}
-
 // NonTerminalSymbol is a non terminal symbol of DSL
 type NonTerminalSymbol struct {
 	Symbol
-}
-
-// NewNonTerminalSymbol is a constructor of NonTerminalSymbol.
-func NewNonTerminalSymbol(symbol string) NonTerminalSymbol {
-	return NonTerminalSymbol{
-		Symbol: Symbol{
-			Text: symbol,
-		},
-	}
 }
 
 // Token is a symbol with arity.
@@ -43,28 +79,18 @@ type Token struct {
 	Arity int
 }
 
-// NewToken is a constructor of Alphabet.
-func NewToken(symbol string, arity int) Token {
-	return Token{
-		Symbol: Symbol{
-			Text: symbol,
-		},
-		Arity: arity,
-	}
-}
-
 func (al *Token) String() string {
 	return al.Text + "[" + strconv.Itoa(al.Arity) + "]"
 }
 
-type rhs struct {
-	Symbol Symbol
+type rightHandSide struct {
+	symbol Token
 	args   []Symbol
 }
 
-func newRhs(symbol Symbol, args ...Symbol) rhs {
-	return rhs{
-		Symbol: symbol,
+func newRightHandSide(symbol Token, args ...Symbol) rightHandSide {
+	return rightHandSide{
+		symbol: symbol,
 		args:   args,
 	}
 }
